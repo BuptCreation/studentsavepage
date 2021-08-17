857a87c0627cfcb04cc87ecb25977235d5ef988e
littlebird-maker <2528357780@qq.com>
2021-08-17 10:22:48 +0800

    学生端用来存储分析数据以及文章的服务端

--- modules/db.js
+++ modules/db.js
@@ -1,12 +1,17 @@
 var mongdbclient=require("mongodb").MongoClient;
-var url="mongodb://localhost:27017"
-var dbname="finally"
+var url="mongodb://47.94.108.20:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+var dbname="BuptCreation"
+
+// var url="mongodb://localhost:27017"
+// var dbname="finally"
+
 
 function connect(callback) {
     mongdbclient.connect(url, { useUnifiedTopology: true },function (err,client) {
         if(err){
             console.log("链接数据库失败！")
         }else{
+            console.log("连接数据库成功")
             var db=client.db(dbname);
             callback&callback(db,client);
         }
--- server/server.js
+++ server/server.js
@@ -6,9 +6,6 @@ var model =require('../modules/db')
 
 
     // Create a web server to serve files and listen to WebSocket connections
-    let app = express();
-    app.use(express.static('static'));
-
     var server=ws.createServer(
         function (conn) {
             conn.on('close',function () {
@@ -21,7 +18,8 @@ var model =require('../modules/db')
                 model.connect(function (db,client) {
                     let flags1=0;
                     let flags2=0;
-                    db.collection("article").updateOne({textno:data.title},{$set:{textno:data.title,content:data.contents}},function (err,ret) {
+                    db.collection("article").updateOne({textno:data.title},{$set:{textno:data.title,            //更新article中的文章
+                            content:data.contents}},function (err,ret) {
                         if(err){
                             console.log("更新出错！",err)
                             flags1=1;
@@ -34,7 +32,7 @@ var model =require('../modules/db')
                     var nums=[]             //用来记录指标
                     var authors=[]          //用来记录作者
 
-                    data.datas.ops.map(function (item,index) {
+                    data.datas.ops.map(function (item,index) {                          //把所有的数据和对应的作者id全部push到nums和authors数组之中
                         if(item.insert[0]!=="\n"){
                             nums.push(item.insert.length)
                             console.log(item.insert.length)
@@ -44,19 +42,38 @@ var model =require('../modules/db')
                     })
                     console.log(authors)
                     console.log(nums)
-                    console.log(data.title)
-                    for (var i=0;i<authors.length;i++){
-                        db.collection("dataarrays").updateOne({textno:data.title},{$set:{authors:authors,contributions:nums}},function (err,ret) {
+                    // console.log(data.title)
+                    var finalauthors=[]   //原本作者的排列顺序
+                    var finalnums=[]      //最终的数量
+                    db.collection("dataarrays").find({textno:data.title}).toArray(function (err,ret) {
                         if(err){
-                            console.log("更新失败！",err)
+                            console.log("查找出错了")
                         }else{
-                            console.log("dataarrays更新成功！")
-                            if(i===authors.length-1){
-                                flags2=1;
+                            finalauthors=ret[0].authors
+                            for(var i=0;i<ret[0].authors.length;i++){
+                                finalnums.push(0)
                             }
+                            for(var i =0;i<authors.length;i++){                                                      //把所有的数据全部按照固定的顺序理出来
+                                for(var j=0;j<finalauthors.length;j++){
+                                    if(authors[i]==finalauthors[j]){
+                                        finalnums[j]=finalnums[j]+nums[i];
+                                        flags=1;
+                                    }
+                                }
+                            }
+
+                            db.collection("dataarrays").updateOne({textno:data.title},{$set:{contributions:finalnums}},function (err,ret) {
+                                if(err){
+                                    console.log("更新失败！",err)
+                                }else{
+                                    console.log("dataarrays更新成功！")
+                                    if(i===authors.length-1){
+                                        flags2=1;
+                                    }
+                                }
+                            })
                         }
-                        })
-                    }
+                    })
                     if(flags1&flags2==1){
                         client.close()
                     }
